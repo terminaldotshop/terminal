32801a6fc4496aca2c617d23e57c12ae981a3a93
Dax Raad <d@ironbay.co>
2024-04-09 13:15:05 -0400

    server

---
+++ go/cmd/ssh/main.go
@@ -0,0 +1,124 @@
+package main
+
+// An example Bubble Tea server. This will put an ssh session into alt screen
+// and continually print up to date terminal information.
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+)
+
+const (
+	host = "0.0.0.0"
+	port = "22"
+)
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			activeterm.Middleware(), // Bubble Tea apps usually require a PTY.
+			logging.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+}
+
+// You can wire any Bubble Tea model up to the middleware with a function that
+// handles the incoming ssh.Session. Here we just grab the terminal info and
+// pass it to the new model. You can also return tea.ProgramOptions (such as
+// tea.WithAltScreen) on a session by session basis.
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	// This should never fail, as we are using the activeterm middleware.
+	pty, _, _ := s.Pty()
+
+	// When running a Bubble Tea app over SSH, you shouldn't use the default
+	// lipgloss.NewStyle function.
+	// That function will use the color profile from the os.Stdin, which is the
+	// server, not the client.
+	// We provide a MakeRenderer function in the bubbletea middleware package,
+	// so you can easily get the correct renderer for the current session, and
+	// use it to create the styles.
+	// The recommended way to use these styles is to then pass them down to
+	// your Bubble Tea model.
+	renderer := bubbletea.MakeRenderer(s)
+	txtStyle := renderer.NewStyle().Foreground(lipgloss.Color("10"))
+	quitStyle := renderer.NewStyle().Foreground(lipgloss.Color("8"))
+
+	m := model{
+		term:      pty.Term,
+		width:     pty.Window.Width,
+		height:    pty.Window.Height,
+		txtStyle:  txtStyle,
+		quitStyle: quitStyle,
+	}
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+// Just a generic tea.Model to demo terminal information of ssh.
+type model struct {
+	term      string
+	width     int
+	height    int
+	txtStyle  lipgloss.Style
+	quitStyle lipgloss.Style
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := fmt.Sprintf("Your term is %s\nYour window size is %dx%d", m.term, m.width, m.height)
+	return m.txtStyle.Render(s) + "\n\n" + m.quitStyle.Render("Press 'q' to quit\n")
+}
--- sst.config.ts
+++ sst.config.ts
@@ -1,11 +1,18 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+import { resolve } from "path";
+
 export default $config({
   app(input) {
     return {
       name: "terminal-shop",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "cloudflare",
+      providers: {
+        aws: true,
+        random: true,
+        docker: true,
+      },
     };
   },
   async run() {
@@ -13,11 +20,9 @@ export default $config({
       $app.stage === "production"
         ? "terminal.shop"
         : $app.stage + ".dev.terminal.shop";
-
     const secrets = {
       SwellSecret: new sst.Secret("SwellSecret"),
     };
-
     const auth = new sst.cloudflare.Auth("Auth", {
       authenticator: {
         link: [secrets.SwellSecret],
@@ -25,26 +30,175 @@ export default $config({
         domain: "auth." + domain,
       },
     });
-
     const api = new sst.cloudflare.Worker("Api", {
       handler: "./packages/workers/src/api.ts",
       link: [secrets.SwellSecret, auth],
       domain: "api." + domain,
     });
+    if (!$dev) {
+      const www = new sst.cloudflare.StaticSite("Www", {
+        domain: "www." + domain,
+        path: "./packages/www",
+        build: {
+          command: "bun run build",
+          output: "./dist",
+        },
+      });
 
-    const www = new sst.cloudflare.StaticSite("Www", {
-      domain: "www." + domain,
-      path: "./packages/www",
-      build: {
-        command: "bun run build",
-        output: "./dist",
-      },
-    });
+      const repository = new aws.ecr.Repository("DockerRepository", {
+        name: [$app.name, $app.stage].join("-"),
+        forceDelete: true,
+      });
 
+      const vpc = new aws.ec2.Vpc("Vpc", {
+        cidrBlock: "10.0.0.0/16",
+        enableDnsSupport: true,
+        enableDnsHostnames: true,
+      });
+      const subnet = new aws.ec2.Subnet("VpcSubnet", {
+        vpcId: vpc.id,
+        cidrBlock: "10.0.1.0/24",
+        mapPublicIpOnLaunch: true,
+      });
+      const igw = new aws.ec2.InternetGateway("VpcIgw", {
+        vpcId: vpc.id,
+      });
+      const routeTable = new aws.ec2.RouteTable("VpcRouteTable", {
+        vpcId: vpc.id,
+        routes: [
+          {
+            cidrBlock: "0.0.0.0/0",
+            gatewayId: igw.id,
+          },
+        ],
+      });
+      const registryInfo = repository.registryId.apply(async (registryId) => {
+        const credentials = await aws.ecr.getCredentials({
+          registryId: registryId,
+        });
+        const decodedCredentials = Buffer.from(
+          credentials.authorizationToken,
+          "base64",
+        ).toString();
+        const [username, password] = decodedCredentials.split(":");
+        return {
+          server: credentials.proxyEndpoint,
+          username: username,
+          password: password,
+        };
+      });
+
+      const image = new docker.Image("SSHImage", {
+        build: {
+          context: resolve("./go"),
+        },
+        imageName: $interpolate`${repository.repositoryUrl}:${$app.stage}`,
+        registry: registryInfo,
+      });
+      new aws.ec2.RouteTableAssociation("VpcRouteTableAssociation", {
+        subnetId: subnet.id,
+        routeTableId: routeTable.id,
+      });
+      const cluster = new aws.ecs.Cluster("Cluster");
+      const executionRole = new aws.iam.Role("SSHRole", {
+        assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+          Service: "ecs-tasks.amazonaws.com",
+        }),
+      });
+      new aws.iam.RolePolicyAttachment("ExecutionRolePolicyAttachment", {
+        role: executionRole.name,
+        policyArn:
+          "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+      });
+      new aws.iam.RolePolicyAttachment("SSHRolePolicyAttachment", {
+        role: executionRole.name,
+        policyArn:
+          "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+      });
+
+      const port = 22;
+      const taskDefinition = new aws.ecs.TaskDefinition("SSHTask", {
+        family: "ssh",
+        trackLatest: true,
+        cpu: "256",
+        memory: "512",
+        networkMode: "awsvpc",
+        requiresCompatibilities: ["FARGATE"],
+        executionRoleArn: executionRole.arn,
+        containerDefinitions: $jsonStringify([
+          {
+            name: "ssh",
+            image: image.repoDigest,
+            portMappings: [
+              {
+                containerPort: port,
+                hostPort: port,
+                protocol: "tcp",
+              },
+            ],
+          },
+        ]),
+      });
+      const sshSecurityGroup = new aws.ec2.SecurityGroup(
+        "SSHNlbSecurityGroup",
+        {
+          vpcId: vpc.id,
+          egress: [
+            {
+              fromPort: 0,
+              toPort: 0,
+              protocol: "-1",
+              cidrBlocks: ["0.0.0.0/0"],
+            },
+          ],
+        },
+      );
+      const nlb = new aws.lb.LoadBalancer("SSHNlb", {
+        internal: false,
+        loadBalancerType: "network",
+        subnets: [subnet.id],
+        enableCrossZoneLoadBalancing: true,
+        securityGroups: [sshSecurityGroup.id],
+      });
+      const targetGroup = new aws.lb.TargetGroup("SSHNlbTargetGroup", {
+        port: port,
+        protocol: "TCP",
+        targetType: "ip",
+        vpcId: vpc.id,
+      });
+      new aws.lb.Listener("SSHListener", {
+        loadBalancerArn: nlb.arn,
+        port: port,
+        protocol: "TCP",
+        defaultActions: [
+          {
+            type: "forward",
+            targetGroupArn: targetGroup.arn,
+          },
+        ],
+      });
+      const service = new aws.ecs.Service("SSHService", {
+        cluster: cluster.arn,
+        taskDefinition: taskDefinition.arn,
+        desiredCount: 1,
+        launchType: "FARGATE",
+        networkConfiguration: {
+          assignPublicIp: true,
+          subnets: [subnet.id],
+          securityGroups: [sshSecurityGroup.id],
+        },
+        loadBalancers: [
+          {
+            targetGroupArn: targetGroup.arn,
+            containerName: "ssh",
+            containerPort: port,
+          },
+        ],
+      });
+    }
     return {
       api: api.url,
       auth: auth.url,
-      www: www.url,
     };
   },
 });
