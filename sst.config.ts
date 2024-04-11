4c90398fa2809ed0cd54a37f48d6c99b4e65ae76
Dax Raad <d@ironbay.co>
2024-04-11 17:41:22 -0400

    sync

--- sst.config.ts
+++ sst.config.ts
@@ -19,6 +19,9 @@ export default $config({
     };
   },
   async run() {
+    const isPermanentStage =
+      $app.stage === "production" || $app.stage === "dev";
+
     const domain =
       $app.stage === "production"
         ? "terminal.shop"
@@ -51,6 +54,41 @@ export default $config({
         output: "./dist",
       },
     });
+
+    if (isPermanentStage) {
+      const github = new aws.iam.OpenIdConnectProvider("GithubOidc", {
+        url: "https://token.actions.githubusercontent.com",
+        clientIdLists: ["sts.amazonaws.com"],
+        thumbprintLists: [
+          "6938fd4d98bab03faadb97b34396831e3780aea1",
+          "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+        ],
+      });
+      const githubRole = new aws.iam.Role("GithubRole", {
+        name: [$app.name, $app.stage, "github"].join("-"),
+        assumeRolePolicy: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Effect: "Allow",
+              Principal: {
+                Federated: github.arn,
+              },
+              Action: "sts:AssumeRoleWithWebIdentity",
+              Condition: {
+                StringLike: github.url.apply((url) => ({
+                  [`${url}:sub`]: "repo:terminalhq/terminal:*",
+                })),
+              },
+            },
+          ],
+        },
+      });
+      new aws.iam.RolePolicyAttachment("GithubRolePolicy", {
+        policyArn: "arn:aws:iam::aws:policy/AdministratorAccess",
+        role: githubRole.name,
+      });
+    }
 
     if (!$dev) {
       const repository = new aws.ecr.Repository("DockerRepository", {
